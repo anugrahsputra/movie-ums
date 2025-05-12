e028d215f428b557e852305dff6818a6727ed6b8
anugrahsputra <anugrahsputra@gmail.com>
2025-05-12 18:45:11 +0700

    feat: Add Constant object with API and base URL
    Introduces a new `Constant` object in `com.downormal.moviesums.core.utils`. This object holds the base URL and API key, retrieved from the `BuildConfig` generated from the `env.properties` file.

---
+++ app/src/main/java/com/downormal/moviesums/core/utils/Constant.kt
@@ -0,0 +1,13 @@
+package com.downormal.moviesums.core.utils
+
+import com.downormal.moviesums.BuildConfig
+
+/**
+ * Code author  : Anugrah Surya Putra.
+ * Project      : MoviesUMS
+ */
+
+object Constant {
+    const val baseUrl = BuildConfig.BASE_URL
+    const val apiKey = BuildConfig.API_KEY
+}
